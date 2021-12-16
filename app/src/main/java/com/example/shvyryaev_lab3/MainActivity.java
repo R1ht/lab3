75f1eaf555cda1fc6ebb9346e4ee466fe145245b
R1ht <vadimka.shvyryaev@mail.ru>
2021-12-16 14:36:47 +0600

    1
    Окно чата с помощью Linear layout'ов

--- app/src/main/java/com/example/shvyryaev_lab3/MainActivity.java
+++ app/src/main/java/com/example/shvyryaev_lab3/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.shvyryaev_lab3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LayoutInflater myInflater = LayoutInflater.from(this);
+        View view = myInflater.inflate(R.layout.activity_main, null);
+    }
+
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+        }
+
     }
 }
